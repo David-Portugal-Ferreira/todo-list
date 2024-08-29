9b292bc583ca38ff927217a2159eaf821a3c5b8d
David-Portugal-Ferreira <davidportugal44@gmail.com>
2024-08-29 15:44:46 +0100

    Add functions to render todos on html, add methods to dom manipulation module

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import * as dom from './modules/dom-manipulation'
 
 const projects = [];
 const defaultProject = new Project('Default');
-
-
-
+const todo1 = new Todo('teste', 'test', 'ded', 8, false, '');
+const todo2 = new Todo('asdf', 'asdf', 'asdf', 13, false, 'asdf');
+defaultProject.addToProject(todo1);
+defaultProject.addToProject(todo2);
 projects.push(defaultProject);
+
 const defaultProject2 = new Project('Default');
 projects.push(defaultProject2);
+
 const defaultProject3 = new Project('Default');
 projects.push(defaultProject3);
 
@@ -28,11 +31,45 @@ function renderProjects() {
     projects.forEach((element, index) => {
         let projButton = dom.createButton();
         projButton.innerText = element.name;
-        elementEvent(projButton, 'click', () => alert('clicou' + index));
+        elementEvent(projButton, 'click', () => loadTodosFromProject(index));
         dom.sidebarContent.appendChild(projButton);
     })
 }
 
 function elementEvent(element, action, func) {
     dom.bindEvent(element, action, func);
+}
+
+function loadTodosFromProject(index) {
+    dom.cleanElement();
+
+    projects[index].items.forEach(element => {
+        const card = dom.createDiv();
+
+        const title = dom.createPhara();
+        title.innerText = element.title;
+        card.appendChild(title)
+
+        const description = dom.createPhara();
+        description.innerText = element.description;
+        card.appendChild(description)
+
+        const dueDate = dom.createPhara();
+        dueDate.innerText = element.dueDate;
+        card.appendChild(dueDate)
+
+        const priority = dom.createPhara();
+        priority.innerText = element.priority;
+        card.appendChild(priority)
+
+        const completed = dom.createPhara();
+        completed.innerText = element.completed;
+        card.appendChild(completed)
+
+        const notes = dom.createPhara();
+        notes.innerText = element.notes;
+        card.appendChild(notes)
+
+        dom.contentTodos.appendChild(card);
+    })
 }
--- src/modules/dom-manipulation.js
+++ src/modules/dom-manipulation.js
@@ -1,6 +1,6 @@
 const wrapper = document.querySelector('.wrapper');
 const sidebarContent = document.querySelector('.sidebar-content');
-const content = document.querySelector('.content');
+const contentTodos = document.querySelector('.content-todos');
 
 function createDiv() {
     const div = document.createElement('div');
@@ -17,14 +17,16 @@ function createButton() {
     return button;
 }
 
-
 function bindEvent (element, action, func) {
     return element.addEventListener(action, func)
+}
+
+function cleanElement() {
+    return contentTodos.replaceChildren();
 }
 
 
 
 
 
-
-export { wrapper, sidebarContent, content, createDiv, createPhara, createButton, bindEvent }
+export { wrapper, sidebarContent, contentTodos, createDiv, createPhara, createButton, bindEvent, cleanElement }
